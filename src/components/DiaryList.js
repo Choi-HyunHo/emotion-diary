38fb301e189a98a827e41abdce6b8d1b1295796e
com <x7788@naver.com>
2022-04-15 18:10:09 +0900

    ✨ 일기 쓰기 버튼 추가

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import MyButton from './MyButton';
 
 const sortOptionList = [
   { value: 'lastest', name: '최신 순' },
@@ -28,6 +30,7 @@ const ControlMenu = ({ value, onChange, optionList }) => {
 };
 
 const DiaryList = ({ diaryList }) => {
+  const navigate = useNavigate();
   // 정렬 기능
   const [sortType, setSortType] = useState('lastest');
   const [filter, setFilter] = useState('all');
@@ -69,6 +72,11 @@ const DiaryList = ({ diaryList }) => {
         value={filter}
         onChange={setFilter}
         optionList={filterOptionList}
+      />
+      <MyButton
+        type={'positive'}
+        text={'새 일기 쓰기'}
+        onClick={() => navigate('/new')}
       />
       {getProcessedDiaryList().map((it) => (
         <div key={it.id}>
