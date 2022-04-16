8d9a6e0903be44917ace663c9ada982dbcdb1490
com <x7788@naver.com>
2022-04-16 16:23:33 +0900

    ✨ Diary - 데이터 가져오기

--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -1,15 +1,62 @@
-import { useParams } from 'react-router-dom';
+import { useContext, useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { DiaryStateContext } from '../App';
+import MyButton from '../components/MyButton';
+import MyHeader from '../components/MyHeader';
+
+import { getStringDate } from '../util/date';
+import { emotionList } from '../util/emotion';
 
 const Diary = () => {
   const { id } = useParams();
-  console.log(id);
+  const diaryList = useContext(DiaryStateContext);
+  const navigate = useNavigate();
 
-  return (
-    <div>
-      <h1>Diary</h1>
-      <p>이곳은 일기 상세 페이지 입니다.</p>
-    </div>
-  );
+  // 상세 페이지에 보여질 데이터
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    if (diaryList.length >= 1) {
+      const targetDiary = diaryList.find(
+        (it) => parseInt(it.id) === parseInt(id)
+      );
+      if (targetDiary) {
+        // 일기가 존재할 때
+        setData(targetDiary);
+      } else {
+        // 일기가 없을 때
+        alert('없는 일기 입니다.');
+        navigate('/', { replace: true });
+      }
+      console.log(targetDiary);
+    }
+  }, [id, diaryList]);
+
+  if (!data) {
+    return <div className="DiaryPage">로딩중 입니다...</div>;
+  } else {
+    const curEmotionData = emotionList.find(
+      (it) => parseInt(it.emotion_id) === parseInt(data.emotion)
+    );
+    console.log(curEmotionData);
+
+    return (
+      <div className="DiaryPage">
+        <MyHeader
+          headText={`${getStringDate(new Date(data.date))} 기록`}
+          leftChild={
+            <MyButton text={'< 뒤로가기'} onClick={() => navigate(-1)} />
+          }
+          rightChild={
+            <MyButton
+              text={'수정하기'}
+              onClick={() => navigate(`/edit/${data.id}`)}
+            />
+          }
+        />
+      </div>
+    );
+  }
 };
 
 export default Diary;
